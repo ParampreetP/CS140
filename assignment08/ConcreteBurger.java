bea5efc65a7944f8e470cc32721407743f157e7b
Parampreet Parmar <39278566+ParampreetP@users.noreply.github.com>
2019-02-13 18:33:30 -0500

    Add files via upload

---
+++ assignment08/ConcreteBurger.java
@@ -0,0 +1,16 @@
+package assignment08;
+
+public class ConcreteBurger extends BurgerAbstraction{
+public ConcreteBurger(BurgerNaming name, BurgerAbstraction next) {
+	super(name, next);
+}
+@Override
+public int getTotalCalories() {
+	return getCalories();
+}
+@Override
+public double getTotalCost() {
+	return getPrice();
+}
+
+}
